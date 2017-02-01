814f80f706b8419b13c566e65b2f84b114ab06ea
Terkel Brix <tbr@rehfeld.dk>
2017-02-01 08:26:15 +0100

    Add Selenium reference Scrape all postnumre to Dic

--- NorthernNerds.Scraper.Boliga/NorthernNerds.Scraper.Boliga/Program.cs
+++ NorthernNerds.Scraper.Boliga/NorthernNerds.Scraper.Boliga/Program.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace NorthernNerds.Scraper.Boliga
 {
@@ -10,6 +13,35 @@ namespace NorthernNerds.Scraper.Boliga
     {
         static void Main(string[] args)
         {
+            Test();
+        }
+
+        public static void Test()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(@"http://www.boliga.dk/maegler/indeks/Sj%C3%A6lland/fritidshus");
+            FindAllePostnumre(driver);
+            driver.Quit();
+        }
+
+        public static void FindAllePostnumre(IWebDriver driver)
+        {
+            var dic = new Dictionary<string, string>();
+            var elemenets = driver.FindElements(By.CssSelector("tbody:nth-child(1)>tr>td>a")).ToList();
+
+
+            var postNrRegex = new Regex(@"\d{4}");
+            var navnRegex = new Regex(@"\D*\s\D*");
+            foreach (var webElement in elemenets)
+            {
+                var postNr = postNrRegex.Match(webElement.Text).Value;
+                var navn = navnRegex.Match(webElement.Text).Value;
+
+                if (!dic.ContainsKey(postNr))
+                {
+                    dic[postNr] = navn.Trim();
+                }
+            }
         }
     }
 }
